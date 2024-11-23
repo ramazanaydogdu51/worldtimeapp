5ad839a0bd62b2e07a54131eee410d60b038d411
ramazanaydogdu51 <51ramazanaydogdu51@gmail.com>
2024-11-23 18:10:22 +0300

    http get method done

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -10,18 +14,22 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-   // var url = Uri.https('https://jsonplaceholder.typicode.com/todos/1');
-    var response= http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-   print(response);
+  void getData () async{
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+    final response = await http.get(url);
+      print(response.body);
 
+     Map dataList= jsonDecode(response.body);
+     print(dataList);
+     print(dataList['title']);
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
+  print('test');
+    getData ();
   }
   @override
   Widget build(BuildContext context) {
