00ed33698a69e83a48a4b89c1ef245ffb1d0459d
ramazanaydogdu51 <51ramazanaydogdu51@gmail.com>
2024-11-28 17:50:57 +0300

    constructor created

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:worldtime/services/world_time.dart';
 
 
 
@@ -14,18 +15,32 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
 
+  String time = 'loading';
+
+  void setupWorldTime()  async {
+    WorldTime instance =WorldTime(location: 'Berlin', flag: 'image', url: 'Berlin');
+    await instance.getTime();
+    print('instance.time' + instance.time);
+    setState(() {
+      time = instance.time;
+    });
+
+  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
   print('test');
-    getTime ();
+    setupWorldTime();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Text('loading screen'),
+    body: Padding(
+    padding:    EdgeInsets.all(50),
+    child: Text(time),
+    ),
     );
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -8,11 +8,14 @@ class WorldTime {
   late String location;
   late String time;
   late String flag;
-   late String url; // location url for the api endpoint
+  late String url; // location url for the api endpoint
 
 
-  void getTime () async{
+  WorldTime ({required this.location, required this.flag, required this.url});
+
+  Future <void> getTime () async{
     final endpoint = Uri.parse('https://timeapi.io/api/time/current/zone?timeZone=Europe%2F$url');
+    print('endpoint = '+ endpoint.toString());
     final response = await http.get(endpoint);
     print(response.body);
 
@@ -22,8 +25,9 @@ class WorldTime {
     DateTime now=  DateTime.parse(dataList['dateTime']);
     print(now);
 
-    now =now.add(Duration(hours: 1 ,minutes: 30));
+    //now =now.add(Duration(hours: 1 ,minutes: 30));
     print(now);
+    time = now.toString();
 
   }
 
