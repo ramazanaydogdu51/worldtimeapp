5047612434b7f783650cb25074744232950e9783
ramazanaydogdu51 <51ramazanaydogdu51@gmail.com>
2024-11-29 16:16:35 +0300

    Passing route Data

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:worldtime/services/world_time.dart';
 
-
-
 class Loading extends StatefulWidget {
   const Loading({super.key});
 
@@ -13,34 +11,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
-
-  String time = 'loading';
-
-  void setupWorldTime()  async {
-    WorldTime instance =WorldTime(location: 'Berlin', flag: 'image', url: 'Berlin');
+  void setupWorldTime() async {
+    WorldTime instance =
+        WorldTime(location: 'Berlin', flag: 'image', url: 'Berlin');
     await instance.getTime();
-    print('instance.time' + instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, "/home", arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
     });
-
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-  print('test');
+    print('test');
     setupWorldTime();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Padding(
-    padding:    EdgeInsets.all(50),
-    child: Text(time),
-    ),
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text('loading'),
+      ),
     );
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -2,35 +2,34 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class WorldTime {
-
   late String location;
   late String time;
   late String flag;
   late String url; // location url for the api endpoint
 
+  WorldTime({required this.location, required this.flag, required this.url});
 
-  WorldTime ({required this.location, required this.flag, required this.url});
+  Future<void> getTime() async {
+    try {
+      final endpoint = Uri.parse(
+          'https://timeapi.io/api/time/current/zone?timeZone=Europe%2F$url');
+      print('endpoint = ' + endpoint.toString());
+      final response = await http.get(endpoint);
+      print(response.body);
 
-  Future <void> getTime () async{
-    final endpoint = Uri.parse('https://timeapi.io/api/time/current/zone?timeZone=Europe%2F$url');
-    print('endpoint = '+ endpoint.toString());
-    final response = await http.get(endpoint);
-    print(response.body);
+      Map dataList = jsonDecode(response.body);
+      print(dataList['dateTime']);
 
-    Map dataList= jsonDecode(response.body);
-    print(dataList['dateTime']);
+      DateTime now = DateTime.parse(dataList['dateTime']);
+      print(now);
 
-    DateTime now=  DateTime.parse(dataList['dateTime']);
-    print(now);
-
-    //now =now.add(Duration(hours: 1 ,minutes: 30));
-    print(now);
-    time = now.toString();
-
+      //now =now.add(Duration(hours: 1 ,minutes: 30));
+      print(now);
+      time = now.toString();
+    } catch (e) {
+      print(e);
+      time ='try catch test';
+    }
   }
-
-
-
 }
